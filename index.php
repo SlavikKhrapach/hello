7098dbcdf2add2908de5a68735c99d840ee0aa10
SlavikKhrapach <hrapachslavik@gmail.com>
2023-04-13 13:48:58 -0400

    Create home page

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Slavik Khrapach
+ * 4/13/2023
+ * 328/hello/index.php
+ * Controller for hello project
+ *
+ */
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require the autoload file
+require_once('vendor/autoload.php');
+
+// Create an F3 (Fat-Free Framework) object
+$f3 = Base::instance();
+
+// Define a default route
+$f3->route('GET /', function() {
+  //echo '<h1>Hello, World!</h1>';
+    // Display view page
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+// Run Fat-Free
+$f3->run();
